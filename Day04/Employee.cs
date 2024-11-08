b03d8012e8da31171ac5b6f695bc682a206f2593
_Pasta <107653769+yryryy@users.noreply.github.com>
2024-11-08 03:58:38 +0200

    Association,Composition and, Aggregation - Properties

---
+++ Day04/Employee.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Day04
+{
+    public class Employee
+    {
+        private int id;
+        private string name;
+        private int age;
+        public decimal Salary { set; get; } // Auto property(The compiler Automatically creates a private data field for the property)
+
+        public int Id // Full Property
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+            }
+        }
+        public string Name { get => name; set => name = value; } 
+        public int Age { get; set; } 
+
+
+        public string Print()
+        {
+            return $"{Id}-{Name}-{Age}-{Salary}";
+        }
+
+        public static Employee AddEmployee(int indx)
+        {
+            Employee tempEmp = new Employee();
+            tempEmp.Id = indx;
+            Console.WriteLine("Please Enter the Name of the Employee");
+            tempEmp.Name =Console.ReadLine();
+
+            int age;
+            Console.WriteLine("Please Enter the Age of the Employee");
+            while (!int.TryParse(Console.ReadLine(), out age))
+            {
+                Console.WriteLine("Please Enter a Valid number for the age");
+            }
+            tempEmp.Age = age;
+
+
+            decimal salary;
+            Console.WriteLine("Please Enter the Salary of the Employee");
+            while (!decimal.TryParse(Console.ReadLine(), out salary))
+            {
+                Console.WriteLine("Please Enter a Valid number for the salary");
+            }
+            tempEmp.Salary = salary;
+            return tempEmp;
+        }
+
+    }
+
+}
---
+++ Day04/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+namespace Day04
+{
+
+    public class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Point(int x = 0, int y = 0)
+        {
+            X = x;
+            Y = y;
+        }
+    /*
+        Commented out Because Property initializer can't be used with Constructor overloading,
+        because this causes ambiguity and the compiler doesn't know which Consutrctor i want to call
+        public Point(int num = 0)
+            {
+                X = num;
+                Y = num;
+            }
+        */
+
+
+        public override string ToString()
+        {
+            return $"({X},{Y})";
+        }
+    }
+
+    // Aggregation : parent(Line, Cirvle, Triangle) can exist independantly of the child (Point)
+
+    public class Line(Point p1 , Point p2 )
+    {
+        public Point P1 { get; set; } = p1;
+        public Point P2 { get; set; } = p2;
+        public override string ToString()
+        {
+            return $"{P1} - {P2}";
+        }
+    }
+
+    public class Tri(Point p1 , Point p2 , Point p3 )
+    {
+        public Point P1 { get; set; } = p1;
+        public Point P2 { get; set; } = p2;
+        public Point P3 { get; set; } = p3;
+        public override string ToString()
+        {
+            return $"{P1} - {P2} - {P3}";
+        }
+    }
+
+    public class Circle(Point center , int radius = 0)
+    {
+        public int Radius { get; set; } = radius;
+        public Point Center { get; set; } = center;
+    }
+
+    // Composition : parent(Rectanle) are Dependant of the child (Point) 
+     
+    
+    public class Rect
+    {
+        public Point UpperLeft { get; private set; }
+        public Point LowerRight{ get; private set; }
+
+        public Rect(int x1,int y1, int x2, int y2)
+        {
+            UpperLeft  = new Point(x1,y1);
+            LowerRight = new Point(x2,y2);
+        }
+        public override string ToString()
+        {
+            return $"UpperLeftPoint: {UpperLeft} - LowerRightPoint: {LowerRight} ";     
+        }
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            // Trying Aggregation
+            Tri triangle = new(new Point(5, 10), new Point(9, 11), new Point(9, 8));
+            Console.WriteLine(triangle);
+
+            Line l = new(new Point(5, 10), new Point(9, 8));
+            Console.WriteLine(l);
+
+            // Trying Composition
+            Rect rectangle = new(0, 5, 5, 0);
+            Console.WriteLine(rectangle);
+
+
+            // Create Object with Property Initializer
+
+            // Point p1 = new Point{ 5, 7 }; // this results in an ERROR, u need to specify the name of the property like in named Parameters
+            Point p1 = new(){ X = 5, Y = 7 }; // this doesn't work if Constructor overloading is used 
+        }
+    }
+}
