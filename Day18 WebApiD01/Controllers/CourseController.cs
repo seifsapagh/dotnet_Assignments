866389e0ca4d1354634d0f26353a750bc7152d92
_Pasta <107653769+yryryy@users.noreply.github.com>
2024-12-03 16:39:58 +0200

    Simple Web Api, getAll, GetByname/Id, Add

---
+++ Day18 WebApiD01/Controllers/CourseController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApi_D01.Models;
+namespace WebApi_D01.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CourseController : ControllerBase
+    {
+        static List<Course> Courses = new()
+        {
+            new Course(){ID=1,Name="Linear Algebra",Duration=5 },
+            new Course(){ID=2,Name="Data structures & Algorithms",Duration=3 },
+            new Course(){ID=3,Name="Discrete Mathmatics",Duration=3 },
+            new Course(){ID=4,Name="Cryptography",Duration=2 },
+            new Course(){ID=5,Name="Networks",Duration=4 }
+        };
+
+        [HttpGet]
+        public List<Course> getAll()
+        {
+            return Courses;
+        }
+        
+        [HttpGet("{name}")]
+        public Course GetByName(string name)
+        {
+            
+            Course course = Courses.Find(c=> c.Name == name);
+
+            return course == null ?  new Course() { ID = 0, Name = "Course NoT Found", Duration = 5 } : course;
+        }
+
+        [HttpPost("add")]
+        public List<Course> AddCourse(Course course)
+        {
+            Courses.Add(course);
+            return Courses;
+        }
+    }
+}
---
+++ Day18 WebApiD01/Models/Course.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApi_D01.Models
+{
+    public class Course
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Duration { get; set; }
+    }
+}
